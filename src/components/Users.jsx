228ddc0e45e2c936ac7a4b7c52bf6668b682d46b
kplnhsyn <kplnhsyn.49@gmail.com>
2023-07-08 09:28:24 +0200

    completed

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,21 +1,48 @@
 import { useState, useEffect } from "react";
 
-
-const getData = () => {
-    fetch("api.github.com/users");
-}
-
 const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    const getData = () => {
+        const url = "https://api.github.com/users";
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => setUsers(data));
+    };
+    // console.log(users);
+
     useEffect(() => {
-        getData()
-    }, [])
-    
+        getData();
+    }, []);
 
-
-
-
-
-    return <div>Users</div>;
+    return (
+        <div>
+            <button  className="btn btn-danger">
+                Click
+            </button>
+            <h1>Users</h1>
+            <div className="row">
+                {users.map((user) => {
+                    // console.log(user);
+                    const { id, login, avatar_url } = user;
+                    return (
+                        <div key={id} className="col">
+                            <div className="card" style={{ width: "18rem" }}>
+                                <img
+                                    src={avatar_url}
+                                    className="card-img-top"
+                                    alt="..."
+                                />
+                                <div className="card-body">
+                                    <h5 className="card-title">{login}</h5>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default Users;
